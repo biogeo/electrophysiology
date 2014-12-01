cbc92f96a9d10c5415708c27f529da5451610078
John Pearson <jmxpearson@gmail.com>
2014-12-01 15:27:34 -0500

    better options in plot_with_sem

--- plot_with_sem.m
+++ plot_with_sem.m
@@ -5,7 +5,7 @@ function plot_with_sem(x, smwid, dt, type, binT, color)
 % mean(x) +/- std(x)/sqrt(N)
 % smwid is a smoothing width in absolute units (smwid = 0 => no smoothing)
 % dt is the time step between successive samples
-% type = 0 for dashed lines, 1 for shaded patch
+% type = 0 for no sem, 1 for shaded patch, 2 for dashed lines
 % binT is the time axis (optional)
 % color (optional) is an rgb triple
 
@@ -39,19 +39,22 @@ end
 
 % now plot
 hold all
-if type == 1
-    Xptch = [binT fliplr(binT)];
-    Yptch = [xlo fliplr(xhi)];
-
-    if ntrials > 1
-        patch(Xptch, Yptch, color, 'Facealpha', ...
-            0.25, 'EdgeColor', 'none'); %black patch with 25% opacity and no border
-    end
-    plot(binT, xsm, 'color', color, 'linewidth',2)
+switch type
+    case 0
+        plot(binT, xsm, 'color', color, 'linewidth', 2);
+    case 1
+        Xptch = [binT fliplr(binT)];
+        Yptch = [xlo fliplr(xhi)];
+        
+        if ntrials > 1
+            patch(Xptch, Yptch, color, 'Facealpha', ...
+                0.25, 'EdgeColor', 'none'); %black patch with 25% opacity and no border
+        end
+        plot(binT, xsm, 'color', color, 'linewidth',2)
     
-else
-    plot(binT, xsm, 'color', color, 'linewidth', 2);
-    plot(binT, xhi, 'color', 0.5*color, 'linestyle', '--', 'linewidth',1)
-    plot(binT, xlo, 'color', 0.5*color, 'linestyle', '--', 'linewidth',1)
+    case 2
+        plot(binT, xsm, 'color', color, 'linewidth', 2);
+        plot(binT, xhi, 'color', 0.5*color, 'linestyle', '--', 'linewidth',1)
+        plot(binT, xlo, 'color', 0.5*color, 'linestyle', '--', 'linewidth',1)
 end
 hold off
